035b123ee759cfc00fff18e170471720787944fd
Dawood <dawood.aijaz97@gmail.com>
2019-08-24 17:40:22 +0500

    #

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ p = (IP(src="35.202.52.252",dst="35.193.17.254")/UDP(sport=80,dport=1234))/Raw(l
 
 send(p,count=2000)
 
-resp = sr1(p,verbose=0,timeout=10,count=20)
+resp = sr1(p,count=20)
 
 print(resp)
 
