97922ed1af25cf7774a2ed6b81ad847f4c878152
Dawood <dawood.aijaz97@gmail.com>
2019-08-24 17:32:39 +0500

    #

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import sys
 from scapy.all import *
 
-p = (IP(src="35.202.52.252",dst="35.193.17.254")/UDP(sport=80,dport=80))/Raw(load="google")
+p = (IP(src="35.202.52.252",dst="35.193.17.254")/UDP(sport=80,dport=1234))/Raw(load="google")
 
-send(p)
+send(p,count=2000)
 
-resp = sr1(p,verbose=0,timeout=10)
+resp = sr1(p,verbose=0,timeout=10,count=20)
 
 print(resp)
 
